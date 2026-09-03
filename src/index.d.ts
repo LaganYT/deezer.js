8c4575eb5e95f74b77d3d781475441f8f8d3f97b
Logan <95306463+LaganYT@users.noreply.github.com>
2026-09-03 12:29:33 -0500

    types: make declarations ESM and Worker-safe

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,17 @@
-declare module "@flazepe/deezer.js" {
-	export type EntityType = "track" | "album" | "artist" | "playlist";
+export type EntityType = "track" | "album" | "artist" | "playlist";
 
-	export interface Entity {
-		type: EntityType;
-		info: Record<string, any>;
-		tracks: Array<Record<string, any>>;
-	}
-
-	export default class Deezer {
-		constructor(arl?: string);
-		api(method: string, body: Record<string, any>): Promise<Record<string, any>>;
-		search(query: string, type?: EntityType): Promise<Array<Record<string, any>>>;
-		get(idOrURL: string, type?: EntityType): Promise<Entity | null>;
-		getAndDecryptTrack(track: Record<string, any>, flac?: boolean): Promise<Buffer>;
-	}
+export interface Entity {
+  type: EntityType;
+  info: Record<string, any>;
+  tracks: Array<Record<string, any>>;
 }
+
+export declare class Deezer {
+  constructor(arl?: string);
+  api(method: string, body: Record<string, any>): Promise<Record<string, any>>;
+  search(query: string, type?: EntityType): Promise<Array<Record<string, any>>>;
+  get(idOrURL: string, type?: EntityType): Promise<Entity | null>;
+  getAndDecryptTrack(track: Record<string, any>, flac?: boolean): Promise<Uint8Array>;
+}
+
+export default Deezer;
